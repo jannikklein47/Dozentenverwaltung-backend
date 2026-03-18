b867737e1468ac4a3785657fa0ac3cdb1599367f
zwergenkoenig21 <hauli15.lr@gmail.com>
2026-03-18 21:50:43 +0100

    add: file

--- routes/v1.0/v1Router.js
+++ routes/v1.0/v1Router.js
@@ -4,9 +4,12 @@ const v1Router = express.Router();
 
 const rWelcome = require("./welcome");
 const appRouter = require("./app/appRouter");
+const authRouter = require("./auth/authRouter");
 
 // Unprotected route
 v1Router.use("/", rWelcome);
+
+v1Router.use("/auth", authRouter);
 
 // Protected routes
 v1Router.use("/app", /*verifyToken(),*/ appRouter);
