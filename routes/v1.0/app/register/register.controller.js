bb063340a1367fadafe8a947e2a127b1cc1227e7
N1ckk0 <nick.opuko@gmail.com>
2026-03-22 23:57:35 +0100

    added:register route

--- routes/v1.0/app/register/register.controller.js
+++ routes/v1.0/app/register/register.controller.js
@@ -1,15 +1,15 @@
-const APIError = require("../../../utils/APIError");
-const { userdaten } = require("../../../models");
+const APIError = require("../../../../utils/error");
+const { User } = require("../../../../models");
 
 exports.register = async (req, res, next) => {
   try {
     const { email, password } = req.body;
 
-    const existingUser = await userdaten.findOne({ where: { email } }); // guckt ob user schon exisitiert
+    const existingUser = await User.findOne({ where: { username: email } }); // E-Mail wird als Username genutzt
     if (existingUser) {
       return next(APIError.errorUserAlreadyExists());
     }
-    const newUser = await userdaten.create({ email, password }); // erstellt neuen user
+    const newUser = await User.create({ username: email, passwort: password }); // erstellt neuen user
 
     res // gibt zurück, dass der user erfolgreich registriert wurde mit der id des neuen users
       .status(201)
