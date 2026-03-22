24b43130c1dcc82eaee0d03133878a4fc13a5fd1
N1ckk0 <nick.opuko@gmail.com>
2026-03-22 23:57:35 +0100

    added:register route

--- routes/v1.0/app/appRouter.js
+++ routes/v1.0/app/appRouter.js
@@ -2,8 +2,10 @@ const express = require("express");
 const appRouter = express.Router();
 const lectureRoutes = require("./lecture/lecture.routes");
 const professorRoutes = require("./professor/professor.routes");
+const registerRoutes = require("./register/register.routes");
 
 appRouter.use("/professors", professorRoutes);
 appRouter.use("/lectures", lectureRoutes);
+appRouter.use("/register", registerRoutes);
 
 module.exports = appRouter;
---
+++ routes/v1.0/app/register/register.validate.js
@@ -0,0 +1,27 @@
+const Joi = require("joi");
+const APIError = require("../../../../utils/error");
+
+const registerSchema = Joi.object({
+  email: Joi.string().email().max(255).required(),
+
+  password: Joi.string()
+    .min(8)
+    .pattern(/[0-9]/, "must contain at least one number")
+    .pattern(/[!@#$%^&*()-+]/, "must contain at least one special character")
+    .pattern(/[A-Z]/, "must contain at least one uppercase letter")
+    .required()
+    .messages({
+      "string.pattern.name": "Password {#name}",
+    }),
+});
+
+exports.validateRegisterBody = (req, res, next) => {
+  const { error, value } = registerSchema.validate(req.body, {
+    stripUnknown: true,
+  });
+  if (error) {
+    return next(APIError.errorValidation(error.message));
+  }
+  req.body = value;
+  next();
+};
