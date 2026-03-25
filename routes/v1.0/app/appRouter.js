921428b19637874780a0135e7a8e99e330716ad2
zwergenkoenig21 <hauli15.lr@gmail.com>
2026-03-25 01:02:02 +0100

    removed auth import in app Router

--- routes/v1.0/app/appRouter.js
+++ routes/v1.0/app/appRouter.js
@@ -2,7 +2,6 @@ const express = require("express");
 const appRouter = express.Router();
 const lectureRoutes = require("./lecture/lecture.routes");
 const professorRoutes = require("./professor/professor.routes");
-const registerRoutes = require("./register/register.routes");
 
 appRouter.use("/professors", professorRoutes);
 appRouter.use("/lectures", lectureRoutes);
