3853706830ab9d7281c8baf0309b0fff7d9c0f77
jannikklein47 <jannikklein0302@outlook.de>
2026-04-08 15:01:01 +0200

    added: user routes, controller, and validation for managing functions

--- routes/v1.0/app/appRouter.js
+++ routes/v1.0/app/appRouter.js
@@ -3,9 +3,11 @@ const appRouter = express.Router();
 const lectureRoutes = require("./lecture/lecture.routes");
 const professorRoutes = require("./professor/professor.routes");
 const reportRoutes = require("./report/report.routes");
+const userRoutes = require("./user/user.routes");
 
 appRouter.use("/professors", professorRoutes);
 appRouter.use("/lectures", lectureRoutes);
 appRouter.use("/reports", reportRoutes);
+appRouter.use("/users", userRoutes);
 
 module.exports = appRouter;
