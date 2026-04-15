71742ba9add673b7470aed357724dc5bf21ab021
zwergenkoenig21 <hauli15.lr@gmail.com>
2026-04-16 00:11:52 +0200

    add files

--- routes/v1.0/app/appRouter.js
+++ routes/v1.0/app/appRouter.js
@@ -3,11 +3,13 @@ const appRouter = express.Router();
 const lectureRoutes = require("./lecture/lecture.routes");
 const professorRoutes = require("./professor/professor.routes");
 const reportRoutes = require("./report/report.routes");
-const userRoutes = require("./user/user.routes");
+const usersRoutes = require("./user/user.routes");
+const singleUserRoutes = require("./singleUser/singleUser.routes");
 
 appRouter.use("/professors", professorRoutes);
 appRouter.use("/lectures", lectureRoutes);
 appRouter.use("/reports", reportRoutes);
-appRouter.use("/users", userRoutes);
+appRouter.use("/users", usersRoutes);
+appRouter.use("/user", singleUserRoutes);
 
 module.exports = appRouter;
