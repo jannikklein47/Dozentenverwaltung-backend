7db510de3b335ab217a1d75b9ac58db8310b8d12
N1ckk0 <nick.opuko@gmail.com>
2026-04-15 08:50:22 +0200

    added: report pm-33

--- routes/v1.0/app/report/report.controller.js
+++ routes/v1.0/app/report/report.controller.js
@@ -11,6 +11,57 @@ const {
 const Logger = require("../../../../utils/logger");
 const { Op } = require("sequelize");
 
+exports.getProfessorsWithoutProvadis = async (req, res, next) => {
+  try {
+    const result = await Dozent.findAll({
+      include: [
+        {
+          model: Vorlesung,
+          as: "lectures",
+          attributes: ["id", "name", "kuerzel", "semester"],
+          required: false,
+          through: {
+            attributes: ["vorlaufzeit", "gehalten_anId"],
+          },
+          include: [
+            {
+              model: Abschluss_Typ,
+              as: "completionType",
+              attributes: ["id", "name"],
+            },
+          ],
+          where: {
+            id: {
+              [Op.notIn]: sequelize.literal(`
+                (
+                  SELECT vd.vorlesungId
+                  FROM Vorlesung_Dozent vd
+                  INNER JOIN Gehalten_An ga ON ga.id = vd.gehalten_anId
+                  WHERE vd.dozentId = Dozent.id
+                    AND ga.name = 'Provadis'
+                )
+              `),
+            },
+          },
+        },
+      ],
+      order: [
+        ["id", "ASC"],
+        [{ model: Vorlesung, as: "lectures" }, "id", "ASC"],
+      ],
+    });
+
+    res.status(200).json(result);
+  } catch (error) {
+    next(
+      new APIError(
+        "Failed to fetch professors and lectures without Provadis",
+        500,
+      ),
+    );
+  }
+};
+
 exports.getLecturewithoutProfessor = async (req, res, next) => {
   try {
     const limit = parseInt(req.query.limit) || 50;
