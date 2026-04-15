41087d9348448020f7e9261296c2239e924e21a3
N1ckk0 <nick.opuko@gmail.com>
2026-04-15 08:52:07 +0200

    added: report format pm33 with swagger

--- routes/v1.0/app/report/report.routes.js
+++ routes/v1.0/app/report/report.routes.js
@@ -249,4 +249,33 @@ router.get(
   reportController.getProfessorWithProvadisLectures,
 );
 
+/**
+ * @swagger
+ * /app/reports/professors/without-provadis:
+ *   get:
+ *     summary: Get all professors with lectures they can teach but have never held at Provadis
+ *     tags: [Reports]
+ *     parameters:
+ *       - in: query
+ *         name: limit
+ *         schema:
+ *           type: integer
+ *         description: Limit number of results
+ *       - in: query
+ *         name: offset
+ *         schema:
+ *           type: integer
+ *         description: Offset for pagination
+ *     responses:
+ *       200:
+ *         description: A list of professors with their lectures excluding lectures already held at Provadis, including Bachelor and Master information
+ *       500:
+ *         description: Server error
+ */
+
+router.get(
+  "/professors/without-provadis",
+  reportController.getProfessorsWithoutProvadis,
+);
+
 module.exports = router;
